34b0ce5a8db947e1c96f4aaca6ccc4aab4e25d0f
Niels <info@heimseiten.de>
2023-03-29 21:50:16 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,3 @@
 <?php
 
-if (TL_MODE == 'FE') {
-    $GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontsroboto/css/contao-google-fonts-roboto-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
-}
+$GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontsroboto/css/contao-google-fonts-roboto-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
